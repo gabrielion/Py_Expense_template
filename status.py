cba1074db8bcb6e07acb807015fa6f7be19042f0
gabriel.lion <gabriel.lion@epita.fr>
2023-09-20 22:01:11 +0200

    status

--- status.py
+++ status.py
@@ -4,7 +4,6 @@ import ast
 def show_status():
     user_balances = {}
 
-    # Read expenses data from a CSV file
     with open('expense_report.csv', mode='r') as file:
         csv_reader = csv.reader(file)
         
@@ -14,13 +13,11 @@ def show_status():
             
             user_balances[spender] = user_balances.get(spender, 0) + float(expense['amount'])
             
-            # Subtract the payback amounts from the balances of users involved in paybacks
             for payback in expense.get('payback', []):
                 user = payback['user']
                 payback_amount = float(payback['payback_amount'])
                 user_balances[user] = user_balances.get(user, 0) - payback_amount
 
-    # Generate the status report
     for user, balance in user_balances.items():
         if balance > 0:
             print(f"{user} owes {balance:.2f}€ to others.")
