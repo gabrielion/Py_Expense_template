bbbb2ca6e8b0ad3cb9c4303d77268f9134c271d5
gabriel.lion <gabriel.lion@epita.fr>
2023-09-20 21:39:02 +0200

    Part 1

--- expense.py
+++ expense.py
@@ -1,30 +1,71 @@
 from PyInquirer import prompt
+from user import get_users
+import csv
 
-expense_questions = [
+users = get_users()
+amount_question = [
     {
         "type":"input",
         "name":"amount",
         "message":"New Expense - Amount: ",
     },
+]
+label_question = [
     {
         "type":"input",
         "name":"label",
         "message":"New Expense - Label: ",
     },
-    {
-        "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
-    },
-
 ]
 
+spender_question = [
+    {
+        "type":"list",
+        "name":"spender",
+        "message":"New Expense - Spender: ",
+        "choices": [user['username'] for user in users]
+    },
+]
+
+def get_splitted_amount(amount, num_involved_users):
+    float_amount = float(amount)
+    split_amount = amount / num_involved_users
+    return split_amount
 
 
 def new_expense(*args):
-    infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    amount = prompt(amount_question)
+    
+    label = prompt(label_question)
+    spender = prompt(spender_question)
+   
+    involved_question = [
+        {
+            "type":"checkbox",
+            "name":"involved_users",
+            "message":"New Expense - involve users: ",
+            "choices": [{'name': user["username"], 'checked': True if user["username"] == spender['spender'] else False } for user in users]
+        },
+    ]
+    involved_users = prompt(involved_question)
+    print(involved_users)
+    paybacks = []
+    for user in involved_users['involved_users']:
+        if user != spender['spender']:
+            payback_amount = float(amount['amount']) / len(involved_users)
+            payback_dict = {
+                'user' : user,
+                'payback_amount': payback_amount,
+                'payback_person': spender['spender']
+            }
+            paybacks.append(payback_dict)
+
+    infos = {**amount, **label, **spender, **{'payback': paybacks}}
+    print(infos)
+
+    with open('expense_report.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([infos])
     print("Expense Added !")
+
     return True
-
-
--- user.py
+++ user.py
@@ -1,8 +1,30 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"username",
+        "message":"New User - Name: ",
+    }
 ]
 
 def add_user():
+    infos = prompt(user_questions)
+    with open('users.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([infos['username']])
     # This function should create a new user, asking for its name
     return
+
+def get_users():
+    users = []
+
+    with open('users.csv', mode='r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            if row:
+                user = {"username": row[0].strip()}  # Assuming the username is in the first column
+                users.append(user)
+
+    return users
